3b4763a7a45290d2311aafb3d8c31c82b16ad931
analuz5 <anaclara.luz339@gmail.com>
2023-10-02 16:15:59 -0300

    Etapas finais

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 document.getElementById('meuFormulario').addEventListener('submit', function(event) {
     event.preventDefault(); // Isso evita a recarga da página
-
+  // Obtenção dos valores dos campos do formulário
     const capital = parseFloat(document.getElementById('capitalInicial').value);
     const juros = parseFloat(document.getElementById('juros').value) / 100;
     const periodo = document.getElementById('periodo').value;
     const tempo = parseFloat(document.getElementById('tempo').value);
     let i = 0;
-
+ // Cálculo da taxa de juros com base no período selecionado
     if (periodo === "a") {
-      i = tempo / 12;
+      i = tempo / 12; //Converte anos para meses
     } else if (periodo === "m") {
-      i = tempo;
+      i = tempo;// Mantém o tempo em meses
     }
+ 
+    const resultadoCalculado = capital * Math.pow(1 + juros, i);// Cálculo do valor final com juros compostos
 
-    const resultadoCalculado = capital * Math.pow(1 + juros, i);
     document.getElementById('resultado').textContent = `O valor final com juros compostos é: ${resultadoCalculado.toFixed(2)}`;
-  });
+  });  // Exibição do resultado no elemento com o ID 'resultado'
